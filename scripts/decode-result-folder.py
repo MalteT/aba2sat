a147e4840c22ccfac03594e898e352d506484764
Malte Tammena <malte.tammena@pm.me>
2025-03-03 13:37:19 +0100

    improve scripts

--- scripts/decode-result-folder.py
+++ scripts/decode-result-folder.py
@@ -126,7 +126,12 @@ def run():
             analysis: Union[Analysis, None] = None
             if os.path.exists(analysis_file):
                 with open(analysis_file, 'r') as f:
-                    analysis = json.load(f)
+                    try:
+                        analysis = json.load(f)
+                    except json.JSONDecodeError as e:
+                        print(f"Error decoding JSON: {analysis_file}")
+                    except Exception as e:
+                        print(f"An unexpected error occurred: {e}")
             (
                 _,
                 atom_count,
