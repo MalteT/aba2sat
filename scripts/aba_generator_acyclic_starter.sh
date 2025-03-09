7b7b32acd87bf655008143e671330d398a8e002c
Malte Tammena <malte.tammena@pm.me>
2025-03-09 20:04:25 +0100

    move scripts elsewhere

--- scripts/aba_generator_acyclic_starter.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=24
-#SBATCH --time=24:00:00
-
-./scripts/aba-generator-acyclic.py
--- scripts/run-gen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --time=10:00:00
-
-./aba-generator-acyclic --directory "$(pwd)/instances"
